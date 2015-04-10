a091a99d612070afc9777fa909941ce8fbf09a1b
Jason Sanford <jasonsanford@gmail.com>
2015-04-10 11:35:27 -0400

    Validate phone number on front-end.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,29 @@
 $(function () {
-  $('.input-phone').on('keyup', updatePhoneLink);
+  var $form = $('.form-phone');
 
-  function updatePhoneLink (event) {
-    var $target = $(event.target);
-    var phone = $target.val();
-    var phone = sanitizePhoneNumber(phone);
-    var phoneFormUrl = '/' + phone;
-    $('.link-phone').attr('href', phoneFormUrl);
-  }
+  var $inputPhone = $('.input-phone');
+
+  $form.on('submit', function (event) {
+    event.preventDefault();
+    var phoneNumber = $inputPhone.val();
+    phoneNumber = sanitizePhoneNumber(phoneNumber);
+
+    if (isValidPhoneNumber(phoneNumber)) {
+      window.location.href = '/' + phoneNumber;
+    } else {
+      window.alert('That phone number doesn\'t look right.');
+    }
+  });
 
   function sanitizePhoneNumber (phone) {
     var sanitized = phone.replace(/-/g, '');
     sanitized = sanitized.replace(/ /g, '');
     sanitized = sanitized.replace(/\./g, '');
+    sanitized = sanitized.trim();
     return sanitized;
+  }
+
+  function isValidPhoneNumber (phoneNumber) {
+    return phoneNumber.length === 10;
   }
 });
