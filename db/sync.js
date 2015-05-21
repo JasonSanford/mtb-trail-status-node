a2d41012d6762753561537fc0d1e8f071cf7ebec
Jason Sanford <jasonsanford@gmail.com>
2015-05-21 11:38:42 -0400

    Don't delete tables on sync.

--- db/sync.js
+++ db/sync.js
@@ -1,16 +1,28 @@
 #!/usr/bin/env node
 
 var request = require('request');
+var _ = require('lodash');
 
 var trails = require('../trails').all;
 var models = require('./models');
 
 var sequelize = models.sequelize;
 
-sequelize.sync({force: true}).then(function () {
+sequelize.sync().then(function () {
   trails.forEach(function (trail) {
-    models.Trail.create(trail).then(function (createdTrail) {
-      console.log('Trail created: ', trail.name);
+    var where = {
+      id: trail.id
+    };
+
+    var defaults = _.clone(trail);
+    delete defaults.id;
+
+    models.Trail.findOrCreate({where: where, defaults: defaults}).spread(function (foundOrCreatedTrail, created) {
+      if (created) {
+        console.log('Trail created: ', trail.name);
+      } else {
+        console.log('Trail found: ', trail.name);
+      }
     }).catch(function (error) {
       throw error;
     });
